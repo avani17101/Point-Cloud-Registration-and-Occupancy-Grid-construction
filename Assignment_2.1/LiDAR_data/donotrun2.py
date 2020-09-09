96c52634d2902b779757e07e31fc05dfaceac961
avani17101 <staravani1999@gmail.com>
2020-09-09 12:50:00 +0530

    resturcture: remove unnecessary files

--- Assignment_2.1/LiDAR_data/donotrun2.py
+++
@@ -1,8 +0,0 @@
-import os
-path = "./01/"
-#()for file in enumerate(os.listdir(path)):
-for i in range(9, -1, -1):
-    print(i)
-    src = "./01/00000" + str(i) + ".bin"
-    dst = "./01/0000" + str(i + 10) + ".bin"
-    os.rename(src, dst)
